2158de1df2297564d712d3bdd4262058b620ac6e
최우영 <djdj7087@naver.com>
2020-04-29 21:26:44 +0900

    first commit

---
+++ src/main/java/com/example/dbmasterspringboot/DbmasterspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbmasterspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmasterspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbmasterspringbootApplication.class, args);
+    }
+
+}
