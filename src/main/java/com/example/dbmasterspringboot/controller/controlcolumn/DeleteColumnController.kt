8841df4b8e1bba96296ac8824800e6a826dfb451
TaeHyeong Kim <uuzaza@navere.com>
2020-06-20 13:45:32 +0900

    #DBMA-310
    delete api 응답코드 명시

--- src/main/java/com/example/dbmasterspringboot/controller/controlcolumn/DeleteColumnController.kt
+++ src/main/java/com/example/dbmasterspringboot/controller/controlcolumn/DeleteColumnController.kt
@@ -27,17 +27,37 @@ class DeleteColumnController(
         val name = response["name"]
         val tableName = response["tableName"]
 
+
         if (tableName == null || name == null || primary_key_name == null || primary_key_value == null) {
             return ResponseDTO("E01","파라미터가 잘못 설정됬습니다.","")
         }
 
+        if (tableName == null || tableName == "") {
+            return ResponseDTO("E01","tableName 값이 입력되지 않았습니다.","")
+        }
+        if (name == null || name == "") {
+            return ResponseDTO("E02","name 값이 입력되지 않았습니다.","")
+        }
+        if (primary_key_name == null || primary_key_name == "") {
+            return ResponseDTO("E03","primary_key_name 값이 입력되지 않았습니다.","")
+        }
+        if (primary_key_value == null || primary_key_value == "") {
+            return ResponseDTO("E04","primary_key_value 값이 입력되지 않았습니다.","")
+        }
         return try {
             val DELET_COLUMN_QUERY = "DELETE FROM $name.$tableName WHERE $primary_key_name = $primary_key_value"
             println(DELET_COLUMN_QUERY)
             jdbcTemplate.execute(DELET_COLUMN_QUERY)
-            ResponseDTO("S01","","")
+
+            ResponseDTO("S01","삭제되었습니다.","")
 
         } catch (e: Exception) {
+            if(e.message!!.contains("doesn't exist")){
+                return ResponseDTO("E05","테이블 또는 데이터베이스가 존재하지 않습니다.","");
+            }
+            if(e.message!!.contains("Unknown column")){
+                return ResponseDTO("E06","칼럼이 존재하지 않습니다.","");
+            }
             resultStr = e.cause.toString()
             ResponseDTO("E01",resultStr,"")
         }
